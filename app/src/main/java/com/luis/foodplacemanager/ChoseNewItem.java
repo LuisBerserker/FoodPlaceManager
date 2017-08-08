6d10bcd430a553d743e4d8fff9d3ae637043b0c7
LuisBerserker <luis.kuhrmann@gmx.de>
2017-08-08 17:34:15 +0200

    Initial commit

---
+++ app/src/main/java/com/luis/foodplacemanager/ChoseNewItem.java
@@ -0,0 +1,20 @@
+package com.luis.foodplacemanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+
+public class ChoseNewItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chose_new_item);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_chose_new_item, IOaquireableFoodsList.ListAquireableFoods(this));
+        ListView listView = (ListView)findViewById(R.id.listOfaquireableFoods);
+        listView.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/luis/foodplacemanager/IOaquireableFoodsList.java
@@ -0,0 +1,43 @@
+package com.luis.foodplacemanager;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.widget.ArrayAdapter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+
+/**
+ * Created by Luis on 08.08.2017.
+ */
+
+public class IOaquireableFoodsList {
+     static ArrayList<String> words = new ArrayList<String>();
+     static   boolean listReReadRequired = true;
+
+    public static String[] ListAquireableFoods(Context context) {
+        if (listReReadRequired) {
+            try {
+                words.clear();
+                AssetManager mngr = context.getAssets();
+                InputStream stream = context.getResources().getAssets().open("aquireableFoods.txt");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    words.add(line);
+                }
+                reader.close();
+                listReReadRequired = false;
+            }
+            catch (IOException ex) {
+            }
+
+        }
+        return words.toArray(new String[0]);
+    }
+
+}
